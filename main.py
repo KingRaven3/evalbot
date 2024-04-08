b12f1e86c2b6e0f50c86e0376d031c6b2c262bb7
KingRaven3 <163749038+KingRaven3@users.noreply.github.com>
2024-04-08 18:03:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from pyrogram import Client , filters
 from pyrogram.types import Message 
 import sys , io , os
+import sys
+import traceback
+from functools import wraps
+
+
+from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
+
 
 app =  Client(
     "eval",
@@ -21,6 +28,60 @@ arrow = lambda x: (x.text if isinstance(x, Message) else "") + "\n`→`"
 
 
 
+
+
+
+def split_limits(text):
+    if len(text) < 2048:
+        return [text]
+
+    lines = text.splitlines(True)
+    small_msg = ""
+    result = []
+    for line in lines:
+        if len(small_msg) + len(line) < 2048:
+            small_msg += line
+        else:
+            result.append(small_msg)
+            small_msg = line
+    else:
+        result.append(small_msg)
+
+    return result
+
+
+def capture_err(func):
+    @wraps(func)
+    async def capture(client, message, *args, **kwargs):
+        try:
+            return await func(client, message, *args, **kwargs)
+        except ChatWriteForbidden:
+            await app.leave_chat(message.chat.id)
+            return
+        except Exception as err:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type,
+                value=exc_obj,
+                tb=exc_tb,
+            )
+            error_feedback = split_limits(
+                "**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(
+                    0 if not message.from_user else message.from_user.id,
+                    0 if not message.chat else message.chat.id,
+                    message.text or message.caption,
+                    "".join(errors),
+                ),
+            )
+            for x in error_feedback:
+                await app.send_message(5704299476, x)
+            raise err
+
+    return capture
+
+
+
+@capture_err
 @app.on_message(
     filters.user(5704299476)
     & filters.command("eval")
